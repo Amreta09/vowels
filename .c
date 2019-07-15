567606df62c4645e93577bc8d561fbac7d43a9d0
Amreta09 <52856519+Amreta09@users.noreply.github.com>
2019-07-15 19:52:52 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int ch;
+scanf("%d",&ch);
+if(ch==a || ch==e || ch==i || ch==o ch==u || ch==A || ch==E || ch==I || ch==O || ch==U)
+{
+printf("Vowels");
+}
+else if((ch>a && ch<z) || (ch>A && ch<Z))
+{
+printf("Consonant");
+}
+else
+{
+printf("Invalid");
+}
+return 0;
+}
